ec83afb365e9265f194451adb8cef08dbb2ad510
KaranChawlaD <karan.chawlad@gmail.com>
2025-01-30 23:26:10 -0500

    CAD Video Added

--- src/pages/StepOne.js
+++ src/pages/StepOne.js
@@ -7,6 +7,9 @@ const StepOne = () => {
       <div>
         <img src="img/stageOne.png" className="bg-image"></img>
         <NavigationButtons prevPage="/" nextPage="/StepTwo" />
+        <video width="640" height="480" loop muted autoPlay controls = ''>
+          <source src="hookcad.mp4" type="video/mp4"></source>
+        </video>
       </div>
   );
 };
