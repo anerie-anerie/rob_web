cc1acd2aaf55aac25d7a1d6a861f8626f46df8fe
Your Name <your.email@example.com>
2025-01-30 22:32:47 -0500

    {message}

--- src/pages/StepFive.js
+++ src/pages/StepFive.js
@@ -1,25 +1,14 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./Steps.css";
 import NavigationButtons from "../components/navigation";
 
 const StepFive = () => {
-    return (
-        <div>
-        {/* Image of the Canva Design */}
-        <img
-          src="/img/stageFive.png" // Correct path
-          alt="Merivale ROB Design1"
-          style={{
-            display: "block",
-            margin: "20px auto",
-            maxWidth: "90%",
-            height: "auto",
-          }}
-        />
-            <NavigationButtons prevPage="/StepFour" nextPage="/StepSix" />
-         </div>
-      );
+  return (
+    <div>
+    <img src="img/stageFive.png" className="bg-image"></img>
+    <NavigationButtons prevPage="/StepFour" nextPage="/StepSix" />
+  </div>
+  );
 };
-
 
 export default StepFive;
